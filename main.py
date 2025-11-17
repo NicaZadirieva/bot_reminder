68fdbbb9f1909819060a6dff93be3d7b40eb5033
NicaZadirieva <no.demchenko@yandex.ru>
2025-11-17 12:53:23 +0300

    feat: добавила репозиторий userRepository

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 ﻿import asyncio
-from app.database.connection import init_db, async_session
-from app.database.models import User, Reminder
+
+from app.database.models import User
+from app.repositories.user_repository import UserRepository
+from app.database.connection import async_session
+
+user_repo = UserRepository()
+
 
 async def main():
-    await init_db()
-    print("✅ БД инициализирована!")
+    async with async_session() as session:
+        #user = await user_repo.create(session, User(username="John", telegram_id=12345))
+
+        user = await user_repo.get_by_id(session, 2)
+    
+        users = await user_repo.get_all(session)
+
+        user = await user_repo.update(session, 2, username="Jane")
+
+        #await user_repo.delete(session, 1)
 
 asyncio.run(main())
