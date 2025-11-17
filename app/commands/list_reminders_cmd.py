a0d951422ac012370b5aca687e609a85b36685bc
NicaZadirieva <no.demchenko@yandex.ru>
2025-11-17 22:44:17 +0300

    feat: добавила команду ListRemindersCommand

--- app/commands/list_reminders_cmd.py
+++ app/commands/list_reminders_cmd.py
@@ -1,1 +1,26 @@
-# Get all list of reminders
+﻿# Get all list of reminders
+from app.database.models import Reminder
+from app.repositories.base import IRepository
+from .base import BotCommand
+from aiogram import types
+from typing import Optional, List, Any
+
+class ListRemindersCommand(BotCommand):
+    def __init__(self, repo: IRepository, session: Any):
+        self.repo = repo
+        self.session = session
+        super().__init__()
+
+    async def printAllReminders(self, reminders: List[Reminder], message: types.Message):
+        answer_text = ""
+        for reminder in reminders:
+            answer_text = answer_text + "\n" + (f'''
+                #{reminder.id} 📝 {reminder.text}
+                ⏰ {reminder.remind_at}
+                🔔 {reminder.status} | 🔄 ${reminder.repeated_value}
+            ''')
+        await message.answer(answer_text)
+
+    async def execute(self, message: types.Message):
+        reminders = await self.repo.get_all(self.session)
+        await self.printAllReminders(reminders, message)
--- main.py
+++ main.py
@@ -3,18 +3,22 @@
 from app.commands.fake_message import FakeMessage
 from app.commands.help_cmd import HelpCommand
 from app.commands.start_cmd import StartCommand
+from app.commands.list_reminders_cmd import ListRemindersCommand
 from app.database.models import Reminder
 from app.repositories.reminder_repository import ReminderRepository
 from app.database.connection import async_session
 from datetime import datetime, timedelta
 
-reminder_repo = ReminderRepository()
-
+from app.repositories.fake_reminder_repository import FakeReminderRepository
 
 async def main():
     helpCmd = HelpCommand()
     await helpCmd.execute(FakeMessage())
     startCmd = StartCommand()
     await startCmd.execute(FakeMessage())
+    async with async_session() as session:
+        reminder_repo = FakeReminderRepository()
+        listAllCmd = ListRemindersCommand(reminder_repo, session)
+        await listAllCmd.execute(FakeMessage())
 
 asyncio.run(main())
