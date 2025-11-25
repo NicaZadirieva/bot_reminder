88814d99dbbb6b97bbf36467ff3768156f892440
NicaZadirieva <no.demchenko@yandex.ru>
2025-11-25 16:05:52 +0300

    feat: добавила вызов команды CreateReminderCommand. еще есть доработки

--- app/commands/create_reminder_cmd.py
+++ app/commands/create_reminder_cmd.py
@@ -1,7 +1,18 @@
 # Create remind
 from .base import BotCommand
+from app.parsers.reminder_parser import ReminderParser
 from aiogram import types
 
 class CreateReminderCommand(BotCommand):
+    def __init__(self):
+        super().__init__()
+        self.parser = ReminderParser()
+
     async def execute(self, message: types.Message):
-        pass
+        user_text = message.text.replace("\remind", "")
+        try:
+            reminder = self.parser.parse(user_text, message.from_user.id)
+            # TODO: создать в бд
+        except:
+            message.answer("Формат данных не соответсвует команде")
+
