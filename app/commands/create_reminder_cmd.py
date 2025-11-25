5fcd877e1c8c73ec982149da106f2ee5caca929d
NicaZadirieva <no.demchenko@yandex.ru>
2025-11-25 16:08:49 +0300

    feat: добавила сохранение в БД

--- app/commands/create_reminder_cmd.py
+++ app/commands/create_reminder_cmd.py
@@ -1,18 +1,22 @@
 # Create remind
 from .base import BotCommand
 from app.parsers.reminder_parser import ReminderParser
+from app.repositories.base import IRepository
 from aiogram import types
+from typing import Optional, List, Any
 
 class CreateReminderCommand(BotCommand):
-    def __init__(self):
+    def __init__(self, repo: IRepository, session: Any):
         super().__init__()
         self.parser = ReminderParser()
+        self.repo = repo
+        self.session = session
 
     async def execute(self, message: types.Message):
         user_text = message.text.replace("\remind", "")
         try:
             reminder = self.parser.parse(user_text, message.from_user.id)
-            # TODO: создать в бд
+            await self.repo.create(self.sesion, reminder);
         except:
             message.answer("Формат данных не соответсвует команде")
 
